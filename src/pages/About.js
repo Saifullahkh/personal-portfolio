319deea253693b18b9a41a4544ba603447e9fee2
Saifullahkh <94992288+Saifullahkh@users.noreply.github.com>
2024-07-23 22:28:02 +0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,17 @@
 import { Box, Button, Grid, Typography } from '@mui/material'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Saif1 from '../Assets/saif1234.jpg'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 function About() {
-
-  useEffect(() => {
-    AOS.init();
-  }, [])
-
   return (
     <>
        <Box sx={{px:10, py:8, '@media (max-width: 600px)': {px:2, py: 3}}} id='about'>
         <Grid container>
-            <Grid item lg={4} data-aos='fade-up-right' >
+            <Grid item lg={4} >
                 <img src={Saif1} alt="" style={{width: '250px', height:'400px', justifyContent: 'center', border: '5px solid #5982f4'}} />
             </Grid>
             <Grid item lg={6}>
-                <Box data-aos='fade-up-left' sx={{py: 8, fontFamily: 'Nunito' , '@media (max-width: 600px)': {py: 4}}}>
+                <Box sx={{py: 8, fontFamily: 'Nunito' , '@media (max-width: 600px)': {py: 4}}}>
                   <Typography sx={{fontSize: '25px', fontWeight: 'bold'}}>About <span style={{color: '#5982f4'}}>Me</span> </Typography>
                   <Typography gutterBottom>Frontend Developer</Typography>
                   <Typography sx={{fontFamily: 'Nunito'}}>
